461be7ec092260c0b2198f3501bd6b6d5cf78068
Cedric Cellier <rixed@happyleptic.org>
2012-07-10 22:04:10 +0200

    Implementation of delete for Red-Black tree
      I followed the footsteps of Jean-Christophe Filliatre's rbset.ml,
      which he proved correct. Yet, and despite the few tests passing,
      I'm convinced there are bugs in my retranscription.

--- pfds_intf.ml
+++ pfds_intf.ml
@@ -94,7 +94,7 @@ sig
     val insert : t -> e -> t
     val member : t -> e -> bool
     val delete : t -> e -> t
-    (** [delete t x] deletes one value that compare equal with x from t *)
+    (** [delete t x] deletes one value that compare equal with x from t, or raise Not_found *)
 end
 
 module type SET =
