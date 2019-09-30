df887e6bdc0436bd6b0746f81a6be0b7bf210e37
Cedric Cellier <rixed@happyleptic.org>
2019-09-30 18:16:06 +0200

    Fix leftist heap merge

--- leftist_heap_impl.ml
+++ leftist_heap_impl.ml
@@ -13,7 +13,6 @@ struct
         | E -> 0
         | T (r, _, _, _) -> r
 
-    (* For some reasons, this merge function is exponential... *)
     let rec merge a b = match a with
         | E -> b
         | T (_, x, a_l, a_r) -> (match b with
@@ -21,8 +20,8 @@ struct
             | T (_, y, b_l, b_r) ->
                 let makeT v l r =
                     let rank_l, rank_r = rank l, rank r in
-                    if rank_l >= rank_r then T (rank_l + 1, v, l, r)
-                    else T (rank_r + 1, v, r, l) in
+                    if rank_l >= rank_r then T (rank_r + 1, v, l, r)
+                    else T (rank_l + 1, v, r, l) in
                 if Ord.compare x y <= 0 then makeT x a_l (merge a_r b)
                 else makeT y b_l (merge a b_r))
 
